e61aca2685b535359f05826410c2a18982c51406
Taylor Lassiter <lassit09@gmail.com>
2017-12-20 01:18:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma(string) {
+  if(whisper(string)) {
+    return "I can't hear you!";
+  } else if(shout(string)) {
+    return "YES INDEED!";
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
